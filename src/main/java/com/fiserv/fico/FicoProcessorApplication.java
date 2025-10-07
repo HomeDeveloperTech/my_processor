670c21d5a9cd3f681d2f366794726499d5a3d75e
victoremerick <emerick_machado@hotmail.com>
2025-10-06 22:48:38 -0300

    Initialize Spring Boot project

---
+++ src/main/java/com/fiserv/fico/FicoProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.fiserv.fico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicoProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FicoProcessorApplication.class, args);
+    }
+}
